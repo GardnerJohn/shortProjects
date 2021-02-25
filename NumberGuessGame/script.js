0394ed0923f005b32b11bd1ed112a4a2a5023f45
GardnerJohn <baguava@gmail.com>
2021-02-25 12:42:09 -0700

    Reset functionality added

--- NumberGuessGame/script.js
+++ NumberGuessGame/script.js
@@ -1,13 +1,13 @@
 'use strict'
 
 // Random number generator
-const ranNum = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = ranNum;
+let ranNum = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 // Check button select and type coercion
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
+    let highScore = 0;
 
     // When no input or not a number
     if (!guess || typeof guess !== 'number') {
@@ -19,7 +19,8 @@ document.querySelector('.check').addEventListener('click', function() {
         // Change background color to green upon win
         document.querySelector('body').style.backgroundColor = 'Green';
         document.querySelector('.number').style.width = '30rem';
-
+        document.querySelector('.number').textContent = ranNum;
+        
     // Guess is too high
     } else if (guess > ranNum) {
         if (score > 1) {
@@ -39,5 +40,16 @@ document.querySelector('.check').addEventListener('click', function() {
         } else {
             document.querySelector('.message').textContent = `Game over. Please try again.`;
         }
-    } 
+    }
+});
+
+// To reset the game
+document.querySelector('.again').addEventListener('click', function() {
+    let ranNum = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('body').style.backgroundColor = "rgb(145, 63, 63)";
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.score').textContent = '20';
+    document.querySelector('.guess').value = '';
 });
